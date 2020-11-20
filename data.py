49752d5f90d0c9553a923101e8895f05ce5b1f49
Abhishek176 <54310761+Abhishek176@users.noreply.github.com>
2020-11-20 16:24:42 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,22 @@
+aws_accounts = {
+    "255482788127": {
+        'category': 'Core',
+        'environment': 'Dev',
+        'alias': 'rsk0418q'
+    },
+    "355482788127": {
+        'category': 'Core',
+        'environment': 'qa',
+        'alias': 'rsk0418q'
+    },
+    "455482788127": {
+        'category': 'Core',
+        'environment': 'Dev',
+        'alias': 'rsk0418q'
+    },
+    "555482788127": {
+        'category': 'Core',
+        'environment': 'qa',
+        'alias': 'rsk0418q'
+    }
+}
